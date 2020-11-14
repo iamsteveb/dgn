bc5218cbe1074fe07ae8ac5eb272faa41d70be55
Steve Baird <steve.baird@hi55.uk>
2020-11-14 19:54:40 +0000

    stuff

--- resources/public/cljs-out/dev/figwheel/main/generated/dev_auto_test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns figwheel.main.generated.dev-auto-test-runner
-  (:require [cljs.test :refer-macros [run-tests]]
-            [cljs-test-display.core] [dgn.room-test] [dgn.core-test]))
-
-  (run-tests (cljs-test-display.core/init! "app-auto-testing") (quote dgn.room-test) (quote dgn.core-test))
